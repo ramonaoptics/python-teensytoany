d9764bd471b3c5353a92e57bc260d6ef7081287b
Mark Harfouche <mark.harfouche@gmail.com>
2019-11-07 17:49:02 -0500

    Add a typo on purpose

--- teensytoany/__init__.py
+++ teensytoany/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Ramona Optics'
 __email__ = 'info@ramonaoptics.com'
-from .teensytoany import TeensyToAny
+from .teensytoany import TeensyToAny_
 
 from ._version import get_versions
 __version__ = get_versions()['version']
