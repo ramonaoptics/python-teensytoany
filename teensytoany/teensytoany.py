dd053ded1097550602504f76f1d608e86a797b15
Mark Harfouche <mark.harfouche@gmail.com>
2019-11-11 11:32:06 -0500

    Major cleanup:
    1. Document GPIO functions.
    2. Better error message when no devices are found.
    3. rename the i2c functions with an i2c prefix

--- teensytoany/teensytoany.py
+++ teensytoany/teensytoany.py
@@ -6,7 +6,12 @@ from serial.tools.list_ports import comports
 from os import strerror
 
 known_devices = [
-    {   # Example device structure
+    # Example device structure
+    # These include useful information about the hardware that is created and
+    # burned in with the serial numbers.
+    # Since the same VID/PID can be assigned to multiple devices,
+    # We must log the individual serial numbers to know what they all do.
+    {
         'serial_number': '4725230',
         'device_name': 'teensytoany',
     },
@@ -31,18 +36,17 @@ known_serial_numbers = [
 
 
 class TeensyToAny:
-    """I've noticed that this is extremely slow. Simply asking the device
-    for the version number seems to take 100 ms exactly.
+    # I've noticed that this is extremely slow. Simply asking the device
+    # for the version number seems to take 100 ms exactly.
 
-    %timeit teensytoany._ask('version')
-    101 ms
+    # %timeit teensytoany._ask('version')
+    # 101 ms
 
-    This was due to it timeing out on the reads instead of stopping when it
-    received the newline character.
+    # This was due to it timeing out on the reads instead of stopping when it
+    # received the newline character.
 
-    Currently, the roundtrip time is about 6ms. This is with version 0.0.1-2
-    of the firmware.
-    """
+    # Currently, the roundtrip time is about 6ms. This is with version 0.0.1-2
+    # of the firmware.
     VID_PID_s = [
         (0x16C0, 0x0483),
     ]
@@ -72,10 +76,9 @@ class TeensyToAny:
         computer but that may not be associated with the TeensyToAny boards.
 
         """
-        devices, _ = zip(
-            *TeensyToAny._device_serial_number_pairs(
-                serial_numbers=serial_numbers)
-        )
+        pairs = TeensyToAny._device_serial_number_pairs(
+            serial_numbers=serial_numbers)
+        devices, _ = zip(*pairs)
         return devices
 
     @staticmethod
@@ -100,59 +103,73 @@ class TeensyToAny:
         computer but that may not be associated with the TeensyToAny boards.
 
         """
-        _, serial_numbers = zip(
-            *TeensyToAny._device_serial_number_pairs(
-                serial_numbers=serial_numbers)
-        )
+        pairs = TeensyToAny._device_serial_number_pairs(
+            serial_numbers=serial_numbers)
+        _, serial_numbers = zip(*pairs)
         return serial_numbers
 
     @staticmethod
     def _device_serial_number_pairs(serial_numbers=None):
         com = comports()
-        return [
+        pairs = [
             (c.device, c.serial_number)
             for c in com
             if ((c.vid, c.pid) in TeensyToAny.VID_PID_s and
                 (serial_numbers is None or
                  c.serial_number in serial_numbers))
         ]
+        if len(pairs) == 0:
+            raise RuntimeError(
+                    "Could not find any device for provided serial number: "
+                    f"{serial_numbers}.")
+        return pairs
 
-    def __init__(self):
-        """Sleep hack times less than 0.004 were found to be unstable with the
-        EEPROM.
+    def __init__(self, serial_number=None, *,
+                 baudrate=115200, timeout=0.1, open=True):
+        """A class to control the TeensyToAny Debugger.
 
-
-        Note
-        -----
-
-        To use the logger, add this at the top of the code before creating the
-        TeensyToAny device.
-
-        >>> import logging
-
-        >>> logger = logging.getLogger('teensytoany.teensytoany')
-        >>> logger.setLevel(logging.DEBUG)
-
-        >>> # create console handler and set level to debug
-        >>> ch = logging.StreamHandler()
-        >>> ch.setLevel(logging.DEBUG)
-        >>> formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        >>> ch.setFormatter(formatter)
-        >>> logger.addHandler(ch)
+        Parameters
+        ----------
+        serial_number: optional
+            If provided, will attempt to open the specified serial number
 
         """
-        # TODO log stuff later I guess??
-        self._logger = logging.getLogger(__name__)
-        port = self.find(self._known_serial_numbers)[0]
-        self._baudrate = 115200
-        self._timeout = 0.100
-        self._serial = Serial(port=port, baudrate=self._baudrate, timeout=self._timeout)
+
+        self._serial_number = serial_number
+        self._baudrate = baudrate
+        self._timeout = timeout
+        self._serial = None
+        if open:
+            self._open()
+
+    def _open(self):
+        if self._serial_number is None:
+            serial_numbers = self._known_serial_numbers
+        else:
+            serial_numbers = [self._serial_number]
+
+        port = self.find(serial_numbers)[0]
+        self._serial = Serial(
+            port=port, baudrate=self._baudrate, timeout=self._timeout)
+
+    def _close(self):
+        if self._serial is not None:
+            self._serial.close()
+
+        self._serial = None
+
+    def __del__(self):
+        # Do we want to call close on this delete instance????
+        self._close()
 
     def _write(self, data) -> None:
         """Write data to the port.
 
         If it is a string, encode it as 'utf-8'.
         """
+
+        if self._serial is None:
+            raise RuntimeError("Device must be opened first")
 
         if isinstance(data, str):
             data = (data + '\n').encode('utf-8')
@@ -167,6 +184,9 @@ class TeensyToAny:
             string of data read.
 
         """
+        if self._serial is None:
+            raise RuntimeError("Device must be opened first")
+
         data = self._serial.read_until(terminator=LF, size=size)
         if decode:
             return data.decode()
@@ -190,8 +210,8 @@ class TeensyToAny:
             message = message.strip()
         return message
 
-    def write_read(self, address: int, data: Sequence, num_bytes: int,
-                   *, retry=10, retry_sleep=0.01) -> Sequence:
+    def i2c_write_read(self, address: int, data: Sequence, num_bytes: int,
+                       *, retry=10, retry_sleep=0.01) -> Sequence:
         if len(data) != 2:
             raise ValueError("data must be of length 2")
         if num_bytes not in [1, 2]:
@@ -212,11 +232,12 @@ class TeensyToAny:
             length=num_bytes, byteorder='big',
             signed=False)
 
-    def write_payload(self, address, payload):
+    def i2c_write_payload(self, address, payload):
         if len(payload) == 1:
             # Trying to write the network chips
             data = int(payload[0])
-            self._ask(f"i2c_write_no_register_uint8 0x{address:02x} 0x{data:02x}")
+            self._ask(
+                f"i2c_write_no_register_uint8 0x{address:02x} 0x{data:02x}")
         elif len(payload) == 3:
             # uint8
             register_address = int.from_bytes(
@@ -224,18 +245,20 @@ class TeensyToAny:
             data = int.from_bytes(
                 payload[2:3], byteorder='big', signed=False)
             self._ask(
-                f"i2c_write_uint8 0x{address:02x} 0x{register_address:04x} 0x{data:02x}")
+                f"i2c_write_uint8 "
+                f"0x{address:02x} 0x{register_address:04x} 0x{data:02x}")
         elif len(payload) == 4:
             register_address = int.from_bytes(
                 payload[0:2], byteorder='big', signed=False)
             data = int.from_bytes(
                 payload[2:4], byteorder='big', signed=False)
             self._ask(
-                f"i2c_write_uint16 0x{address:02x} 0x{register_address:04x} 0x{data:04x}")
+                f"i2c_write_uint16 "
+                f"0x{address:02x} 0x{register_address:04x} 0x{data:04x}")
         else:
             raise NotImplementedError()
 
-    def read_bytes(self, address, num_bytes=1):
+    def i2c_read_bytes(self, address, num_bytes=1):
         if num_bytes != 1:
             raise NotImplementedError()
         returned = self._ask(f"i2c_read_no_register_uint8 0x{address:02x}")
@@ -246,11 +269,44 @@ class TeensyToAny:
             signed=False)
 
     def gpio_digital_write(self, pin, value):
+        """Call the ardunio DigitalWrite function.
+
+
+        Parameters
+        ----------
+        pin: int
+            Pin number to control.
+        value: 0, 1, "HIGH", "LOW"
+            Value to assign to the pin
+
+        """
         self._ask(f"gpio_digital_write {pin} {value}")
 
     def gpio_pin_mode(self, pin, mode):
+        """Call the arduino PinMode function.
+
+        Parameters
+        ----------
+        pin: int
+            Pin number to control.
+        mode: 0, 1, "INPUT", "OUTPUT"
+            Mode which to set the pin to.
+        """
         self._ask(f"gpio_pin_mode {pin} {mode}")
 
     def gpio_digital_read(self, pin):
+        """Call the arduino DigitalRead function.
+
+        Parameters
+        ----------
+        pin: int
+            Pin number to read.
+
+        Returns
+        -------
+        value: int, 0, 1
+            Read value.
+
+        """
         returned = self._ask(f"gpio_digital_read {pin}")
         return bool(int(returned, base=0))
