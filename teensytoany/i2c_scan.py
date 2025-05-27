ae37a6ced37e20391a21aa7d93a59f17b4d4ca2c
Mark Harfouche <mark.harfouche@gmail.com>
2025-05-26 20:53:52 -0400

    Add release note and verbose mode

--- teensytoany/i2c_scan.py
+++ teensytoany/i2c_scan.py
@@ -10,22 +10,24 @@ from teensytoany import TeensyToAny
               help='I2C interface to use (0 for I2C, 1 for I2C1).')
 @click.option('--seven-bit-mode', '-7', is_flag=True, default=False,
               help='Report addresses in 7-bit mode instead of 8-bit mode.')
+@click.option('--verbose', is_flag=True, default=False,
+              help='Report in verbose mode, print out all pinged addresses.')
 @click.version_option(teensytoany.__version__)
 def main(
     serial_number=None,
     interface=0,
     seven_bit_mode=False,
+    verbose=False,
 ):
     i2c_scan(
         serial_number=serial_number,
         interface=interface,
         seven_bit_mode=seven_bit_mode,
+        verbose=verbose,
     )
 
 
-def i2c_scan(serial_number=None, interface=0, seven_bit_mode=False):
-    teensy = TeensyToAny(serial_number=serial_number)
-
+def _scan_and_print(teensy, interface, seven_bit_mode, verbose):
     for address_7bit in range(1, 128):
         address = address_7bit << 1
         try:
@@ -38,9 +40,20 @@ def i2c_scan(serial_number=None, interface=0, seven_bit_mode=False):
             else:
                 print(f"Found device at address 0x{address:02X}")
         finally:
-            pass
+            if verbose and seven_bit_mode:
+                print(f"No device found at addr 0x{address_7_bit:02X}")
+            elif verbose and not seven_bit_mode:
+                print(f"No device found at addr 0x{address:02X}")
+            # else not verbose:
+            #     pass
 
-    teensy.close()
+
+def i2c_scan(serial_number=None, interface=0, seven_bit_mode=False, verbose=False):
+    teensy = TeensyToAny(serial_number=serial_number)
+    try:
+        _scan_and_print(teensy, interface=interface, seven_bit_mode=seven_bit_mode)
+    finally:
+        teensy.close()
 
 
 if __name__ == "__main__":
